96580a66b4c4ad8cdd9c81a49c986ffb3e085646
Ryan Garren <rryangarren@gmail.com>
2021-03-26 08:52:00 -0600

    eCommerce half completed

---
+++ e_commerce/inserts.sql
@@ -0,0 +1,15 @@
+INSERT INTO users (name, email) 
+VALUES 
+('ryan', 'ryan@gmail.com');
+
+INSERT INTO users (name, email) 
+VALUES 
+('elizabeth', 'elizabeth@gmail.com'),
+('kelsey', 'kelsey@gmail.com');
+
+INSERT INTO products
+(name, price)
+VALUES
+('stick', 100),
+('puck', 5),
+('goal', 200);
